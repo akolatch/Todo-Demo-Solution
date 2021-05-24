c7f7d590e979e9b901630d0ad5514dd6a5602dc3
akolatch <aaron.kolatch@gmail.com>
2021-05-24 10:39:45 -0400

    update core httmll

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './styles.css';
 
-var mountNode = document.getElementById('app');
-ReactDOM.render(<App />, mountNode);
